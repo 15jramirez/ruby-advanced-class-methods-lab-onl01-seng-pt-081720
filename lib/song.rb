4f521cbd6d7d91dfb9a164180a5f3f86dbe34b6e
Jocelyn  Ramirez <ramirez.jocelyn73@yahoo.com>
2020-09-14 04:41:27 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -46,10 +46,10 @@ class Song
     end
     
  def self.new_from_filename(filename)
-   new_file = filename.delete_suffix(".mp3").split("-")
+   new_file = filename.delete_suffix(".mp3").split(" -")
    song = self.new
    song.name = new_file[1]
-   song.artist_name = new_file[0]
+   song.artist_name =new_file[0]
    song.save
    song
  end
